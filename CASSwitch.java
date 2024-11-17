6471337a282bbba17715eef88a9eee76019122f4
suhithak-12 <116929993+suhithak-12@users.noreply.github.com>
2024-11-17 16:04:12 -0700

    Update CASSwitch.java

--- CASSwitch.java
+++ CASSwitch.java
@@ -1,1 +1,6 @@
-
+/*
+Core switch in a star
+handles traffic, global traffic forwards to the CCS switch
+implements frame forwarding logic, local firewall rules that are recieved from CCS switch
+supports multiple connections through threading 
+*/
