e5147022cbb7565212c67285539d79ec686a03db
suhithak-12 <116929993+suhithak-12@users.noreply.github.com>
2024-11-17 17:19:19 -0700

    Create CCSSwicth.java

---
+++ CCSSwicth.java
@@ -0,0 +1,8 @@
+/*
+Central core switch for the SoSs network
+creates a bridge bwtween CAS swicthes for global communication
+implements fire wall rules (globally)
+ACKs for global traffic
+sends local firewall rules to CAS swicthes
+Handles traffic bw CAS switches
+*/
